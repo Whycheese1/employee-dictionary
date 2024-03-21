ab080975dfa51b79cd321df09ee84c24e5bbee0f
Whycheese1 <160049443+Whycheese1@users.noreply.github.com>
2024-03-21 12:05:46 -0600

    Add files via upload

---
+++ Employee dict.py
@@ -0,0 +1,43 @@
+###########################
+#Employee Dictionary
+#Name : Jarrett Sanderson
+#Date : March 21 2024
+#Task: Complete functions with the provided info
+###########################
+
+import easygui as easy
+
+#Create a nest dictionary
+employee = {{"name":"Slater",
+            "job":"teacher",
+            "location": "CCHS",
+            "start_date": 2018},
+            {"name":"Tomporowski",
+             "job":"teacher",
+             "location":"CCHS",
+             "start_date": 2000}}
+
+def add_person():
+    #Get job info: name,job,location,start_date 
+    #Create local dictionary
+    #Add to global dictionary
+    
+employee.update({"name":"Tomporowski",
+                 "job":"teacher",
+                 "location":"CCHS",
+                 "start_date": 2000})
+    
+def print_employees():
+    #Print all NAMES from global dictionary
+    
+    
+def print_job_location_pairs():
+    #Print out ALL jobs and their locations
+    
+    
+def print_possible_raise():
+    #If employee started prior to 2020,
+    #eligible for a raise, print out data
+    #for individual
+    
+    
